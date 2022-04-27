2f2d3cde54bbedafd1e240bdd307b7c1fcbf0c63
makaiyahnolen <s_makaiyah.nolen@ousd.org>
2022-04-27 09:11:48 -0700

    wednesday

--- app/routes/default.py
+++ app/routes/default.py
@@ -2,10 +2,15 @@ from app import app
 from flask import render_template
 
 # This is for rendering the home page
+
 @app.route('/')
 def index():
-    return render_template('index.html')
+    return render_template('/index.html')
 
 @app.route('/aboutus')
 def aboutus():
     return render_template('aboutus.html')
+
+@app.route('/getmotivation')
+def getmotivation():
+    return render_template('getmotivation.html')
